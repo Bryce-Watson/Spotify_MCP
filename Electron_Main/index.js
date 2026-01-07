a849ac36b9fecffd189c6b548b0710a4b25dcbef
ApoTh30sis <Brylanbrylan2@gmail.com>
2026-01-07 03:10:37 -0700

    Updated Index so it will Preload Handlers for ICP.

--- Electron_Main/index.js
+++ Electron_Main/index.js
@@ -1,15 +1,28 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow} = require('electron')
+const path = require("node:path");
+// Main Window
+let mainWindow ;
+
+// Initialize Handlers
+const spotHandlers = require("./Handlers/SpotifyHandlers")
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
+
     //autoHideMenuBar: true,
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'Preload.js')
+    }
   })
-
-  win.loadURL('http://localhost:5173/')
+    console.log(mainWindow)
+  mainWindow.loadURL('http://[::1]:5173/')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    // Register Handlers here
+    console.log(mainWindow)
+    createWindow() // create window first so that mainWindow can be sent to the handlers
+    spotHandlers.registerHandlers(mainWindow);
 })
