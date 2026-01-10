3f99d4fd7154856a8e16e3a4a5e2a81a52f60133
ApoTh30sis <Brylanbrylan2@gmail.com>
2026-01-10 12:16:27 -0700

    All interfaces for the renderer to use, has access to all IPC's

---
+++ renderer/src/All-Interfaces.ts
@@ -0,0 +1,25 @@
+export interface OllamaIPC {
+askOllama: (userInput: string) => Promise<void>,
+checkOllamaSetup: () => Promise<boolean>,
+}
+
+export interface SpotifyIPC {
+    checkToken: () => Promise<boolean>,
+    spotUserAuth: (data: string) => Promise<boolean>,
+    getSpotPFP: () => Promise<string>,
+    authSuccess: (callback: () => void) => void,
+    authFailure: (callback: () => void) => void,
+}
+
+export interface GeneralIPC {
+    displayText: (callback: (event: Event, text:string) => void) => void,
+}
+
+declare global {
+    interface Window {
+        ollamaIPC: OllamaIPC
+        spotifyIPC: SpotifyIPC
+        generalIPC: GeneralIPC
+    }
+}
+
