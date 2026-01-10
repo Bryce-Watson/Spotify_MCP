3b36f589dcec02f27c105b8ec8e9ed21e8cb6ccf
ApoTh30sis <Brylanbrylan2@gmail.com>
2026-01-10 12:15:20 -0700

    added OllamaHandlers.js initialization.

--- Electron_Main/index.js
+++ Electron_Main/index.js
@@ -5,6 +5,7 @@ let mainWindow ;
 
 // Initialize Handlers
 const spotHandlers = require("./Handlers/SpotifyHandlers")
+const ollamaHandlers = require("./Handlers/OllamaHandlers")
 
 const createWindow = () => {
     mainWindow = new BrowserWindow({
@@ -16,13 +17,12 @@ const createWindow = () => {
       preload: path.join(__dirname, 'Preload.js')
     }
   })
-    console.log(mainWindow)
   mainWindow.loadURL('http://[::1]:5173/')
 }
 
 app.whenReady().then(() => {
     // Register Handlers here
-    console.log(mainWindow)
     createWindow() // create window first so that mainWindow can be sent to the handlers
     spotHandlers.registerHandlers(mainWindow);
+    ollamaHandlers.registerHandlers(mainWindow);
 })
