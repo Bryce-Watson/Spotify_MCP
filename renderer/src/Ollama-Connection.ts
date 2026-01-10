ce1ebd04b0904ea70c563fadc65577c2cad63c10
ApoTh30sis <Brylanbrylan2@gmail.com>
2026-01-10 12:13:39 -0700

    Code for the ollama button on renderer.

---
+++ renderer/src/Ollama-Connection.ts
@@ -0,0 +1,45 @@
+const ollamaConnectionButton = document.getElementById('ollama-connect-button') as HTMLButtonElement;
+const sendButton = document.getElementById('send-button') as HTMLButtonElement;
+const promptInput = document.getElementById('prompt-input') as HTMLInputElement;
+const aiResponse = document.getElementById('response-placeholder') as HTMLParagraphElement;
+const ollamaStatusLabel = document.getElementById('ollama-status-label-text') as HTMLSpanElement;
+
+let debounce = false;
+let userConnected = false
+
+window.generalIPC.displayText(async (_event:Event, text:string) => {
+    if (debounce) return;
+    debounce = true;
+    aiResponse.innerHTML = ""
+    for (let i = 0; i < text.length; i++) {
+        await new Promise(resolve => setTimeout(resolve, 10))
+        const char = text[i]
+        if (char === '\n') {
+            aiResponse.innerHTML += '<br>'
+        } else {
+            aiResponse.innerHTML += char
+        }
+    }
+    debounce = false;
+})
+
+
+
+sendButton.addEventListener('click', async () => {
+    if (userConnected) { // check if ollama is setup
+        console.log("send button clicked")
+        const input = promptInput.value
+        promptInput.value = ""
+        await window.ollamaIPC.askOllama(input)
+    }
+})
+
+ollamaConnectionButton.addEventListener('click', async () => {
+    const ollamaIsSetup = await window.ollamaIPC.checkOllamaSetup()
+    if (ollamaIsSetup) {
+        userConnected = true
+        ollamaStatusLabel.textContent = "Connected"
+        ollamaConnectionButton.classList.add("hidden")
+        document.documentElement.style.setProperty('--ollamaConnectionColor', '#53d769');
+    }
+})
